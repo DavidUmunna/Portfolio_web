264fda32d57dc602bbc08fca7b4ddef2919474b1
chima1968 <chimarokeumunna98@gmail.com>
2024-12-25 00:46:28 +0100

    fade-in

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 document.addEventListener('DOMContentLoaded', ()=>{
-    const heading = document.querySelector('.section_1 h1');
+    const heading = document.querySelector('.section_1 h1 ');
+    const paragraph = document.querySelector('.section_1 p');
     heading.classList.add('fade-in');
+    paragraph.classList.add('fade-in_1')
 });
