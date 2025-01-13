1c2c4e45a6dc94e1cfa831f2d962d8df03fda3db
chima1968 <chimarokeumunna98@gmail.com>
2025-01-13 09:45:48 +0100

    new commit

--- js/app.js
+++ js/app.js
@@ -1,16 +1,41 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+class page_functionality{
+    constructor(){
+        this.elements= document.querySelectorAll('a[href^="#"]')
+        //this.link= document.querySelector(this.getAttribute('href'))
+        this.heading=document.querySelector('.section_1 h1 ')
+        this.paragraph=document.querySelector('.section_1 p');
+        this.init()
+    }
+    init(){
+        this.scroll()
+        this.fade_in()
+    }
+    scroll(){
+        this.elements.forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+    
+                this.link.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
         });
-    });
-});
+    }
+    fade_in(){
+        document.addEventListener('DOMContentLoaded', ()=>{
+        const heading=document.querySelector(this.getAttribute('href'))
+        console.log(heading)
+        heading.classList.add('fade-in');
+        this.paragraph.classList.add('fade-in_1')
+        });
+    }
+}
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    const heading = document.querySelector('.section_1 h1 ');
-    const paragraph = document.querySelector('.section_1 p');
-    heading.classList.add('fade-in');
-    paragraph.classList.add('fade-in_1')
-});
+const app=new page_functionality()
+
+
+
+
+
+
